5b7da409d2ce3806d83a0cdd1370e78a5703a258
Benjamin Damkjær Østner <benj4539@zbc.dk>
2020-08-05 13:38:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopenhagenAirportH3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CopenhagenAirportH3Entities ctx = new CopenhagenAirportH3Entities())
+            {
+
+                foreach (Airport airport in ctx.Airports)
+                {
+                    Console.WriteLine("IATA: "+airport.IATA);
+                    Console.WriteLine("Name: "+airport.Name);
+
+                    foreach (Airline routeOwner in airport.Airlines)
+                    {
+                        Console.WriteLine("Owner: "+routeOwner.Name);
+                    }
+
+                    foreach (Airline route in airport.Airlines1)
+                    {
+                        Console.WriteLine("Route: "+route.Name);
+                    }
+                    Console.WriteLine("------------");
+                }
+
+                Console.ReadKey();
+            };
+            
+        }
+    }
+}
